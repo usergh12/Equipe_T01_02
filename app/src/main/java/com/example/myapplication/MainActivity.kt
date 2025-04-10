6335eced51fba3c43a19cfa7ec1fa5b4f94e8294
ZozeLuis <paodebatataquerocafe@gmail.com>
2025-04-10 20:11:37 -0300

    Primeiras telas prontas

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import ButtonClickHandler
+import TextViewClickHandler
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val txtCadastro = findViewById<TextView>(R.id.txtCadastro)
+        val txtClicker = TextViewClickHandler(this, TelaCadastro::class.java)
+        val btnEntrar = findViewById<Button>(R.id.btnLogin)
+        val btnHandler = ButtonClickHandler(this, TelaRegistro::class.java)
+        val txtErro = findViewById<TextView>(R.id.txtFalhaLogin)
+
+        txtClicker.irParaTela(txtCadastro)
+        btnHandler.erroLogin(btnEntrar, txtErro)
+
+    }
+}
